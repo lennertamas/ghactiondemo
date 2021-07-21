cddbd5dfd8b7364065af7f3a9e2653fb9e9f6a2a
lennertamas <tamas.lenner@gmail.com>
2021-07-21 11:18:55 +0200

    Second commit

--- src/test/java/WikiTest.java
+++ src/test/java/WikiTest.java
@@ -22,7 +22,7 @@ public class WikiTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         webdriver = new ChromeDriver(options);
 
         webdriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
