6fa070c28fb16a3cc3bd5cf57b1fd277fde11e22
lennertamas <tamas.lenner@gmail.com>
2021-07-29 15:44:16 +0200

    Second commit

--- src/test/java/WikiTest.java
+++ src/test/java/WikiTest.java
@@ -1,11 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.ByteArrayInputStream;
 import java.util.concurrent.TimeUnit;
 
 public class WikiTest {
@@ -36,8 +36,8 @@ public class WikiTest {
         englishButton.click();
         String expectedTitle = "Welcome to Wikipedia,";
         WebElement titleDiv = webdriver.findElement(By.xpath("/html/body/div[3]/div[3]/div[5]/div[1]/div[1]/div/div[1]"));
+        Allure.addAttachment("screenshot", new ByteArrayInputStream(((TakesScreenshot)webdriver).getScreenshotAs(OutputType.BYTES)));
         Assertions.assertEquals(expectedTitle, titleDiv.getText());
-
     }
 
     @Test
