0a0ed3e3f13619bffe774bcb5a028be383559b24
lennertamas <tamas.lenner@gmail.com>
2021-07-29 15:26:24 +0200

    Second commit

---
+++ src/test/java/JustForFunTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JustForFunTest {
+    @Test
+    public void TestFalse()
+    {
+        Assertions.assertTrue(false);
+    }
+}
